6ff7dd010206b4305897a45d39940c5445c02dad
周帅 <xlzhxz@126.com>
2017-10-19 11:25:20 +0800

    c++

--- hello_class/mian.cpp
+++ hello_class/mian.cpp
@@ -10,6 +10,19 @@ private:
 	int age;
 	double height;
 public:
+	Animal() 
+	{
+		name = "a";
+		age = 9;
+		height = 1.23;
+	}
+	Animal(string name,int age,double height) 
+	{
+		this->name = name;
+		this->age = age;
+		this->height = height;
+	}
+
 	void set_name(string name)
 	{
 		this->name = name; //右边name是传过来的参数,左边是成员		
@@ -57,12 +70,20 @@ int main()
 	b.show();
 
 
-	Animal* c = new Animal();   //new之后要主动回收内存，在main之中肯定会被回收
-	c->set_name("机器猫");
+	Animal* c = new Animal("musk",45,1.85);   //new之后要主动回收内存，在main之中肯定会被回收。new是不带参数的构造函数
+	/*c->set_name("机器猫");
 	c->set_age(20);
-	c->set_height(112.3);
+	c->set_height(112.3);*/
+	c->show();
+	c->set_age(46);
 	c->show();
 	delete c;    //回收内存。不能释放一次内存后再次释放
+
+	Animal* d = new Animal("xing", 45, 1.85);  
+	d->show();
+	
+	cout << d->get_name() << endl;
+	delete c;
 	
 	cin.get();
 	cin.get();
