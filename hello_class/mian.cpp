8d45a1b8a6cbfbcddbda9b35017c6448e5da2386
周帅 <xlzhxz@126.com>
2017-10-19 12:54:06 +0800

    c++

--- hello_class/mian.cpp
+++ hello_class/mian.cpp
@@ -10,6 +10,63 @@ private:
 	int age;
 	double height;
 public:
+	Animal();
+	Animal(string name, int age, double height);
+	void set_name(string name);
+	string get_name();
+	void set_age(int age);
+	int get_age();
+	void set_height(double height);
+	double get_height();
+public:
+	void show();
+};
+Animal::Animal() 
+{
+	name = "hai";
+	age = 9;
+	height = 2.31;
+}
+Animal::Animal(string name, int age, double height)
+{
+	this->name = name;
+	this->age = age;
+	this->height = height;
+}
+
+void Animal::set_name(string name)
+{
+	this->name = name; //右边name是传过来的参数,左边是成员
+}
+string Animal::get_name()
+{
+	return this->name;
+}
+void Animal::set_age(int age)
+{
+	this->age = age;
+}
+int Animal::get_age()
+{
+	return this->age;
+}
+void Animal::set_height(double height)
+{
+	this->height = height;
+}
+double Animal::get_height()
+{
+	return this->height;
+}
+void Animal::show()
+{
+	cout << name << " " << age << " "<< height << " " << endl;
+}
+/*private:
+	string name;  //我们把字符串当作对象时（有值的对象实体），我们用string。而我们把它当类时（需要字符串类中定义的方法），我们用String
+	int age;
+	double height;
+public:
 	Animal() 
 	{
 		name = "a";
@@ -55,6 +112,7 @@ public:
 		cout << height << " " << endl;
 	}
 };
+*/
 
 int main()
 {
@@ -75,14 +133,16 @@ int main()
 	c->set_age(20);
 	c->set_height(112.3);*/
 	c->show();
+	c->set_name("Alice");
 	c->set_age(46);
 	c->show();
 	delete c;    //回收内存。不能释放一次内存后再次释放
 
-	Animal* d = new Animal("xing", 45, 1.85);  
+	Animal* d = new Animal("xing", 35, 1.66);  
 	d->show();
-	
-	cout << d->get_name() << endl;
+	c->set_name("Ben");
+	d->set_age(36);
+	d->show();
 	delete c;
 	
 	cin.get();
